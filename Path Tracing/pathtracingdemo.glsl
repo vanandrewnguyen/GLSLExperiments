b5f12af9baca6ff84f2bfb5668a52e87d9e3d312
Van Andrew <andrew.nguyen25@outlook.com>
2022-04-23 12:47:31 +1000

    Update pathtracingdemo.glsl

--- Path Tracing/pathtracingdemo.glsl
+++ Path Tracing/pathtracingdemo.glsl
@@ -6,6 +6,68 @@ Van Andrew Nguyen
 Dabbled into path-tracing, I wanted to try learn sample theory and other methods of rendering scenes.
 */
 
+// Utility
+// Credits: https://blog.demofox.org/2020/06/06/casual-shadertoy-path-tracing-2-image-improvement-and-glossy-reflections/
+vec3 LessThan(vec3 f, float value) {
+    return vec3(
+        (f.x < value) ? 1.0f : 0.0f,
+        (f.y < value) ? 1.0f : 0.0f,
+        (f.z < value) ? 1.0f : 0.0f);
+}
+ 
+vec3 LinearToSRGB(vec3 rgb) {
+    rgb = clamp(rgb, 0.0f, 1.0f);
+     
+    return mix(
+        pow(rgb, vec3(1.0f / 2.4f)) * 1.055f - 0.055f,
+        rgb * 12.92f,
+        LessThan(rgb, 0.0031308f)
+    );
+}
+ 
+vec3 SRGBToLinear(vec3 rgb) {
+    rgb = clamp(rgb, 0.0f, 1.0f);
+     
+    return mix(
+        pow(((rgb + 0.055f) / 1.055f), vec3(2.4f)),
+        rgb / 12.92f,
+        LessThan(rgb, 0.04045f)
+    );
+}
+
+// ACES tone mapping curve fit to go from HDR to LDR
+//https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+vec3 ACESFilm(vec3 x) {
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+    return clamp((x*(a*x + b)) / (x*(c*x + d) + e), 0.0f, 1.0f);
+}
+
+// Shlick approx
+float FresnelReflectAmount(float n1, float n2, vec3 normal, vec3 incident, float f0, float f90) {
+        // Schlick aproximation
+        float r0 = (n1-n2) / (n1+n2);
+        r0 *= r0;
+        float cosX = -dot(normal, incident);
+        if (n1 > n2)
+        {
+            float n = n1/n2;
+            float sinT2 = n*n*(1.0-cosX*cosX);
+            // Total internal reflection
+            if (sinT2 > 1.0)
+                return f90;
+            cosX = sqrt(1.0-sinT2);
+        }
+        float x = 1.0-cosX;
+        float ret = r0+(1.0-r0)*x*x*x*x*x;
+ 
+        // adjust reflect multiplier for object reflectivity
+        return mix(f0, f90, ret);
+}
+
 // Buffer A
 #define SURFDIS 0.01
 #define MAXDIS 100.0
@@ -15,6 +77,30 @@ Dabbled into path-tracing, I wanted to try learn sample theory and other methods
 #define FOV 90.0
 
 // Credits to https://blog.demofox.org/2020/05/25/casual-shadertoy-path-tracing-1-basic-camera-diffuse-emissive/
+
+struct materialData {
+    // emissive = how much light is cast from this object
+    // albedo = base colour of the object
+    // specInt = percentage of light reflected
+    // roughness = blurriness of reflection
+    // specCol = colour of the specular reflection, e.g. coloured metals
+    // IOR = index of refraction
+    vec3 emissive;
+    vec3 albedo;
+    float specInt;
+    float roughness;
+    vec3 specCol;
+    float IOR;
+};
+
+// Keeps data of the ray in a struct
+struct rayHitData {
+    float dist;
+    vec3 normal;
+    materialData matData;
+};
+
+// Randomises the seed used to bounce rays off surfaces
 uint wangHash(inout uint seed) {
     // Hash func by Thomas Wang
     seed = uint(seed ^ uint(61)) ^ uint(seed >> uint(16));
@@ -29,6 +115,7 @@ float hash11(inout uint seed) {
     return float(wangHash(seed)) / 4294967296.0;
 }
 
+// Generates a random unit vector along the positive hemisphere
 vec3 generateRandomUnitVector(inout uint seed) {
     // Remember we are influence the seed for every ray bounce, hence it's an inout var
     float z = hash11(seed) * 2.0 - 1.0;
@@ -39,19 +126,12 @@ vec3 generateRandomUnitVector(inout uint seed) {
     return vec3(x, y, z);
 }
 
-// Keeps data of the ray in a struct
-struct rayHitData {
-    float dist;
-    vec3 normal;
-    vec3 emissive;
-    vec3 albedo;
-};
-
 float scalarTriple(vec3 u, vec3 v, vec3 w) {
     // Find normal of u, v vectors and get mag on vector w
     return dot(cross(u, v), w);
 }
 
+// For indepth analysis on the ray-sphere intersections check out: https://www.youtube.com/watch?v=8fWZM8hCX5E
 // Intersection funct for sphere
 bool testQuadTrace(in vec3 rayPos, in vec3 rayDir, inout rayHitData data, in vec3 a, in vec3 b, in vec3 c, in vec3 d) {
     // calculate normal and flip vertices order if needed
@@ -175,8 +255,8 @@ void scene(in vec3 rayPos, in vec3 rayDir, inout rayHitData hitInfo) {
         vec3 D = vec3(wallLen,  wallLen, farPoint);
         if (testQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
-            hitInfo.albedo = vec3(0.1f, 0.7f, 0.1f);
-            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+            hitInfo.matData.albedo = vec3(0.1f, 0.7f, 0.1f);
+            hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f);
         }
     } 
     
@@ -188,8 +268,8 @@ void scene(in vec3 rayPos, in vec3 rayDir, inout rayHitData hitInfo) {
         vec3 D = vec3(-wallLen,  wallLen, farPoint);
         if (testQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
-            hitInfo.albedo = vec3(0.7f, 0.1f, 0.1f);
-            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+            hitInfo.matData.albedo = vec3(0.7f, 0.1f, 0.1f);
+            hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f);
         }
     } 
     
@@ -201,8 +281,8 @@ void scene(in vec3 rayPos, in vec3 rayDir, inout rayHitData hitInfo) {
         vec3 D = vec3(-wallLen, wallLen, nearPoint);
         if (testQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
-            hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
-            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+            hitInfo.matData.albedo = vec3(0.7f, 0.7f, 0.7f);
+            hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f);
         }
     } 
     
@@ -214,8 +294,8 @@ void scene(in vec3 rayPos, in vec3 rayDir, inout rayHitData hitInfo) {
         vec3 D = vec3(-wallLen, -wallLen, nearPoint);
         if (testQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
-            hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
-            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+            hitInfo.matData.albedo = vec3(0.7f, 0.7f, 0.7f);
+            hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f);
         }
     } 
     
@@ -227,39 +307,48 @@ void scene(in vec3 rayPos, in vec3 rayDir, inout rayHitData hitInfo) {
         vec3 D = vec3(-wallLen,  wallLen, farPoint);
         if (testQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
-            hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
-            hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+            hitInfo.matData.albedo = vec3(0.7f, 0.7f, 0.7f);
+            hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f);
         }
     } 
     
     // Lighting
     float pad = 2.0;
     {
-        vec3 A = vec3(-wallLen + pad, wallLen - 0.1, farPoint - pad);
-        vec3 B = vec3( wallLen - pad, wallLen - 0.1, farPoint - pad);
-        vec3 C = vec3( wallLen - pad, wallLen - 0.1, nearPoint + pad);
-        vec3 D = vec3(-wallLen + pad, wallLen - 0.1, nearPoint + pad);
+        vec3 A = vec3(-wallLen + pad*2.0, wallLen - 0.1, farPoint - pad);
+        vec3 B = vec3( wallLen - pad*2.0, wallLen - 0.1, farPoint - pad);
+        vec3 C = vec3( wallLen - pad*2.0, wallLen - 0.1, nearPoint + pad);
+        vec3 D = vec3(-wallLen + pad*2.0, wallLen - 0.1, nearPoint + pad);
         if (testQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
-            hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
-            hitInfo.emissive = vec3(1.0f, 0.9f, 0.7f) * 20.0;
+            hitInfo.matData.albedo = vec3(0.7f, 0.7f, 0.7f);
+            hitInfo.matData.emissive = vec3(1.0f, 0.9f, 0.7f) * 20.0;
         }
     } 
 
     // Balls
-    if (testSphereTrace(rayPos, rayDir, hitInfo, vec4(-5.0f, -5.0f, 20.0f, 2.0f))) {
-        hitInfo.albedo = vec3(1.0f, 0.8f, 0.8f);
-        hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);        
+    if (testSphereTrace(rayPos, rayDir, hitInfo, vec4(-5.0f, -4.0f, 20.0f, 2.0f))) {
+        hitInfo.matData.albedo = vec3(1.0f, 0.8f, 0.8f);
+        hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f);  
+        hitInfo.matData.specInt = 0.9;
+        hitInfo.matData.roughness = 0.0;
+        hitInfo.matData.specCol = hitInfo.matData.albedo;
     } 
      
-    if (testSphereTrace(rayPos, rayDir, hitInfo, vec4(0.0f, -5.0f, 20.0f, 2.0f))) {
-        hitInfo.albedo = vec3(0.8f, 1.0f, 0.8f);
-        hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);        
+    if (testSphereTrace(rayPos, rayDir, hitInfo, vec4(0.0f, -6.0f, 20.0f, 2.0f))) {
+        hitInfo.matData.albedo = vec3(0.8f, 1.0f, 0.8f);
+        hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f); 
+        hitInfo.matData.specInt = 0.5;
+        hitInfo.matData.roughness = 0.5;
+        hitInfo.matData.specCol = hitInfo.matData.albedo;
     }    
      
-    if (testSphereTrace(rayPos, rayDir, hitInfo, vec4(5.0f, -5.0f, 20.0f, 2.0f))) {
-        hitInfo.albedo = vec3(0.8f, 0.8f, 1.0f);
-        hitInfo.emissive = vec3(0.0f, 0.0f, 0.0f);
+    if (testSphereTrace(rayPos, rayDir, hitInfo, vec4(5.0f, -8.0f, 20.0f, 2.0f))) {
+        hitInfo.matData.albedo = vec3(0.8f, 0.8f, 1.0f);
+        hitInfo.matData.emissive = vec3(0.0f, 0.0f, 0.0f);
+        hitInfo.matData.specInt = 0.0;
+        hitInfo.matData.roughness = 0.0;
+        hitInfo.matData.specCol = hitInfo.matData.albedo;
     }
      
     // Light
@@ -270,8 +359,8 @@ void scene(in vec3 rayPos, in vec3 rayDir, inout rayHitData hitInfo) {
         vec3 D = vec3(-wallLen,  wallLen, nearPoint);
         if (testQuadTrace(rayPos, rayDir, hitInfo, A, B, C, D))
         {
-            hitInfo.albedo = vec3(0.7f, 0.7f, 0.7f);
-            hitInfo.emissive = vec3(1.0f, 0.9f, 0.7f) * 20.0;
+            hitInfo.matData.albedo = vec3(0.7f, 0.7f, 0.7f);
+            hitInfo.matData.emissive = vec3(1.0f, 0.9f, 0.7f) * 20.0;
         }
     }    
 }
@@ -298,11 +387,30 @@ vec3 getRayColour(in vec3 startRayPos, in vec3 startRayDir, inout uint randomSee
          
         // Update ray position and update its direction along the normal
         rayPos = (rayPos + rayDir * hitInfo.dist) + hitInfo.normal * SURFDIS;
-        rayDir = normalize(hitInfo.normal + generateRandomUnitVector(randomSeed));        
+        
+        // We need to make a differentiation between rays that are for diff / spec lighting
+        float chanceSpec = hitInfo.matData.specInt;
+        if (chanceSpec > 0.0) {
+            chanceSpec = FresnelReflectAmount(1.0, hitInfo.matData.IOR, 
+                         rayDir, hitInfo.normal, hitInfo.matData.specInt, 1.0);
+        }
+        float chooseSpec = (hash11(randomSeed) < chanceSpec) ? 1.0 : 0.0;
+        float chooseProbability = (chooseSpec == 1.0) ? chanceSpec : 1.0 - chanceSpec;
+        chooseProbability = max(chooseProbability, 0.001);
+        
+        // Smooth spec uses the reflection ray
+        // Diff uses a random ray along the positive hemisphere
+        // Rough specular interpolates diff and spec
+        vec3 diffRayDir = normalize(hitInfo.normal + generateRandomUnitVector(randomSeed));
+        vec3 specRayDir = reflect(rayDir, hitInfo.normal);
+        float roughness = hitInfo.matData.roughness * hitInfo.matData.roughness;
+        specRayDir = normalize(mix(specRayDir, diffRayDir, roughness)); //squaring roughness to make it seem more linear
+        rayDir = mix(diffRayDir, specRayDir, chooseSpec); //normalize(hitInfo.normal + generateRandomUnitVector(randomSeed));        
          
         // Add in emissive colour of the hit, then update the bounced colour
-        col += hitInfo.emissive * throughput;
-        throughput *= hitInfo.albedo;      
+        col += hitInfo.matData.emissive * throughput;
+        throughput *= hitInfo.matData.albedo;    
+        throughput /= chooseProbability;
     }
 
     return col;
@@ -318,7 +426,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     // Setting up camera (target is on -1->1 map)
     float camDis = 1.0 / tan(FOV * 0.5 * PI / 180.0);
     vec3 rayPos = vec3(0.0f, 0.0f, 0.0f);
-    vec3 rayTarget = vec3(uv * 2.0f - 1.0f, camDis);
+    // Anti aliasing -> adding a random vector on rayTarget on the imaginary plane
+    vec2 targetOffset = vec2(hash11(randomSeed), hash11(randomSeed)) - 0.5;
+    vec3 rayTarget = vec3(((fragCoord + targetOffset) / iResolution.xy) * 2.0f - 1.0f, camDis);
     
     // Changing aspect ratio
     float ratio = iResolution.x / iResolution.y;
@@ -335,7 +445,18 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 
 // Image Out
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+    
     vec2 uv = fragCoord / iResolution.xy;
     vec3 col = texture(iChannel0, uv).rgb;
+    
+    // Colour conversions, because rgb is not true to human eyes
+    // First we convert high dynamic range to a normalised value, since lights are unbounded 1->?
+    // Then we convert our linear colours to SRGB
+    float exposure = 0.5;
+    col *= exposure;
+    
+    col = ACESFilm(col);
+    col = LinearToSRGB(col);
+    
     fragColor = vec4(col, 1.0);
 }
